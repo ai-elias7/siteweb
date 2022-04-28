7821254ab51aa6fe9147ef24abca5cdd35ddf11f
jsnin9 <103622164+jsnin9@users.noreply.github.com>
2022-04-28 12:12:30 +0200

    Update var.php

--- var.php
+++ var.php
@@ -35,3 +35,72 @@ foreach( $_SESSION["categories"] as $products ){
     
 
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+ session_start();
+    $_SESSION["categories"] = array(
+
+                "T01" => array(
+                    "id"=>0,
+                    "img"=>"../img/phillips.png",
+                    "name"=>"Body noir en jersey à col asymétrique",
+                    "price"=>"19,99",
+                    "stock"=>"10"
+                ),
+
+                "T02" => array(
+                    "id"=>0,
+                    "img"=>"../img/phillips.png",
+                    "name"=>"Body noir en jersey à col asymétrique",
+                    "price"=>"19,99",
+                    "stock"=>"10"
+                ),
+                "T03" => array(
+                    "id"=>0,
+                    "img"=>"../img/phillips.png",
+                    "name"=>"Body noir en jersey à col asymétrique",
+                    "price"=>"19,99",
+                    "stock"=>"10"
+                ),
+    );
+;
+
+foreach( $_SESSION["categories"] as $products ){
+    echo $products["name"];
+}
+
+    
+
+?>
